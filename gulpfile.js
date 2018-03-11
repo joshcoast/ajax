d82358cc91cb185a3a2cb54b2b8feac9580d9f7d
Josh Coast <josh@culturefoundry.com>
2018-03-11 11:48:13 -0600

    Install browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
 
-gulp.task('hello', function() {
-  console.log('Hello Zell');
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
 });
